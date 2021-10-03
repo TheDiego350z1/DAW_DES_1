05ab17198d2f3536f0f34f58e106465368f4166b
Diego Garcia <diego350z1@gmail.com>
2021-10-02 22:20:00 -0600

    add .js .html

---
+++ index.js
@@ -0,0 +1,10 @@
+let today = new Date();
+
+/* 
+    Date() devuelve los valores de los meses:
+    0 - Enero, 1 - Febrero, 2 - Marzo, 3 - Abril, 4 - Mayo ...
+*/
+
+let month  = today.getMonth();
+
+console.log(month);
